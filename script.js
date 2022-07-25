0459e079076facdc8917d546ad299e7c2ff86492
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-07-25 18:58:27 -0300

    Update button selector

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const memeImageInput = document.querySelector('[data-js="image-input"]');
 const memeContainer = document.querySelector('[data-js="meme-container"]');
 const memeImage = document.querySelector('[data-js="meme-image"]');
 const memeText = document.querySelector('[data-js="meme-text"]');
-const borderButtonsContainer = document.querySelector('[data-js="border-buttons-container"]');
+const borderButtonsContainer = document.querySelector(
+  '[data-js="border-buttons-container"]'
+);
 const memesImagesContainer = document.querySelector('[data-js="memes-images"]');
 
 memeTextInput.addEventListener('input', (event) => {
@@ -22,24 +24,27 @@ memeImageInput.addEventListener('change', () => {
 const changeBorder = (borderStyle) => {
   const lastItem = memeContainer.classList.length - 1;
   const lastClass = memeContainer.classList[lastItem];
-  memeContainer.className = memeContainer.className.replace(lastClass, borderStyle);
+  memeContainer.className = memeContainer.className.replace(
+    lastClass,
+    borderStyle
+  );
 };
 
 borderButtonsContainer.addEventListener('click', (event) => {
   const dataAttribute = event.target.dataset.js;
 
   switch (dataAttribute) {
-  case 'fire-button':
-    changeBorder('border-red-500');
-    break;
-  case 'water-button':
-    changeBorder('border-blue-500');
-    break;
-  case 'earth-button':
-    changeBorder('border-green-500');
-    break;
-  default:
-    break;
+    case 'fire-button':
+      changeBorder('border-red-500');
+      break;
+    case 'water-button':
+      changeBorder('border-blue-500');
+      break;
+    case 'earth-button':
+      changeBorder('border-green-500');
+      break;
+    default:
+      break;
   }
 });
 
